d0c977ce28bdf2338fe3f724df582cd79eff1964
kenchan0130 <tt.tanishi100@gmail.com>
2024-04-13 21:45:29 +0900

    change linter tool from ESLint & Prettier to Biome

--- src/getosAsync.ts
+++ src/getosAsync.ts
@@ -2,5 +2,5 @@ import getos from "getos";
 
 export const getosAsync = (): Promise<getos.Os> =>
   new Promise((resolve, reject) =>
-    getos((e, os) => (e ? reject(e) : resolve(os)))
+    getos((e, os) => (e ? reject(e) : resolve(os))),
   );
